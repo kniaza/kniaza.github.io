4ac1cac326d9e77d87b53e96df512a5ce46fb6a0
Guram Niazashvili <forhitin@gmail.com>
2016-04-05 14:18:01 +0400

    update

--- morse/morse.js
+++ morse/morse.js
@@ -60,19 +60,67 @@ window.onload = function(){
             '(': '-.--.-',
             '\"': '.-..-.',
             '@': '.--.-.',
-            '=': '-...-'
+            '=': '-...-',
+            '\u00A0': ' / ' // this.alphabet["\u00A0"]
         };
 
         this.init();
     }
-    MorseTranslator.prototype.init = function(){
-        
+    MorseTranslator.prototype.init = function (){
+        this.clickTextToMorse();
+        this.clickMorseToText();
     };
 
-    MorseTranslator.prototype.textToMorse = function(){
+    MorseTranslator.prototype.textToMorse = function (text){
+        var __self = this;
+        text = text.split('');
+
+        var result = text.map(function(item){
+            if(item === " " || item === '\n') item = "\u00A0";
+            else item.toLowerCase();
+            return __self.alphabet[item];
+        });
+
+        return result.join(' ');
+    };
+
+    MorseTranslator.prototype.clickTextToMorse = function (){
+        var __self = this;
+
+        this.domElem.TtoM.onclick = function(){
+            var text = __self.domElem.text.value;
+            __self.domElem.MorT.value = __self.textToMorse(text);
+        };
+
+    };
+
+    MorseTranslator.prototype.morseToText = function (text){
+        var __self = this;
+        text = text.split(' ');
+        var result = text.map(function(item){
+            if(item === '/') {
+                item = '\u00A0';
+            } else {
+                for(var i in __self.alphabet){
+                    if(__self.alphabet[i] === item){
+                        item = i;
+                    }
+                }
+            }
+
+            return item;
+        });
+        return result.join('');
+    };
+
+    MorseTranslator.prototype.clickMorseToText = function (){
+        var __self = this;
 
 
-
+        this.domElem.MtoT.onclick = function (){
+            var text = __self.domElem.text.value;
+            __self.domElem.MorT.value = __self.morseToText(text);
+        };
     };
 
 
